3d7853a94bc21ba9d0f4ab5f952217ad6cc5fea5
monax-owo <zprl.dev@gmail.com>
2025-01-21 14:24:56 +0900

    fix

--- src/subscription.rs
+++ src/subscription.rs
@@ -6,9 +6,16 @@ use tray_icon::menu::{MenuEvent, MenuId};
 
 pub fn tray_menu_listener() -> impl Stream<Item = MenuId> {
   stream::channel(16, |mut output| async move {
+    let (sender, mut reciever) = tokio::sync::mpsc::channel(16);
+
+    std::thread::spawn(move || loop {
+      if let Ok(event) = MenuEvent::receiver().recv() {
+        sender.blocking_send(event).unwrap()
+      }
+    });
+
     loop {
-      if let Ok(MenuEvent { id }) = MenuEvent::receiver().recv() {
-        println!("send");
+      if let Some(MenuEvent { id }) = reciever.recv().await {
         output.send(id).await.unwrap();
       }
     }
