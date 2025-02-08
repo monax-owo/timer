cf5173bafe2a87c1a0400e57d3d00a81dfd0ed6b
monax-owo <zprl.dev@gmail.com>
2025-02-08 22:13:00 +0900

    update config::view

--- src/app/view/config.rs
+++ src/app/view/config.rs
@@ -1,5 +1,21 @@
+use crate::app::config::ConfigEvent;
+
 use super::*;
 
 pub(super) fn view(_app: &App) -> Element<Message> {
-  { container(text("Config")).center(Fill) }.into()
+  {
+    Column::new()
+      .push(text("Config").center())
+      .push(
+        Row::new()
+          .push(button(text("Load")).on_press(Message::ConfigEvent(ConfigEvent::Load)))
+          .push(button(text("Save")).on_press(Message::ConfigEvent(ConfigEvent::Save)))
+          .align_y(Center)
+          .spacing(4),
+      )
+      .width(Fill)
+      .align_x(Center)
+      .spacing(4)
+  }
+  .into()
 }
