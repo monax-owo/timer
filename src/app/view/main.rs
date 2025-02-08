9e8e4c6b7406d14405c09d01b2f3e51f6ddb00a7
monax-owo <zprl.dev@gmail.com>
2025-02-08 13:43:06 +0900

    update ui

--- src/app/view/main.rs
+++ src/app/view/main.rs
@@ -16,8 +16,9 @@ pub(super) fn view(app: &App) -> Element<Message> {
       .push(button(pause).on_press(Message::Pause(app.timer.enable)))
       .push(
         Row::new()
-          .push(text(app.config.check_rate.as_secs()))
+          .push(text(app.config.check_rate.as_secs()).center().width(32))
           .push(container(check_rate_slider.width(Fill)).padding([0, 12]))
+          .push(Space::with_width(32))
           .align_y(Center)
           .padding([0, 8]),
       )
