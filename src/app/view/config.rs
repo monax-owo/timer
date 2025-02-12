cd8a8beb7b2cf8ff5eac7e92b5df365a89af1535
monax-owo <zprl.dev@gmail.com>
2025-02-12 17:35:54 +0900

    up config::view

--- src/app/view/config.rs
+++ src/app/view/config.rs
@@ -3,17 +3,23 @@ use crate::app::config::ConfigEvent;
 use super::*;
 
 pub(super) fn view(app: &App) -> Element<Message> {
+  let configs = Column::new()
+    .push(pick_list(Theme::ALL, Some(&app.current_theme), Message::ChangeTheme).text_size(12))
+    .push(button("Notify").on_press(Message::Notify));
+
   {
     Column::new()
       .push(text("Configs").center())
-      .push(pick_list(Theme::ALL, Some(&app.current_theme), Message::ChangeTheme).text_size(12))
-      .push(button("Notify").on_press(Message::Notify))
+      .push(scrollable(configs.spacing(8)).height(Fill))
       .push(
-        Row::new()
-          .push(button(text("Load")).on_press(Message::ConfigEvent(ConfigEvent::Load)))
-          .push(button(text("Save")).on_press(Message::ConfigEvent(ConfigEvent::Save)))
-          .align_y(Center)
-          .spacing(4),
+        container(
+          Row::new()
+            .push(button(text("Load")).on_press(Message::ConfigEvent(ConfigEvent::Load)))
+            .push(button(text("Save")).on_press(Message::ConfigEvent(ConfigEvent::Save)))
+            .align_y(Center)
+            .spacing(4),
+        )
+        .align_right(Fill),
       )
       .width(Fill)
       .align_x(Center)
