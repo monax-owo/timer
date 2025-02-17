cad6191b03d9f1e13df64b3234c33f4af9e9cb44
monax-owo <zprl.dev@gmail.com>
2025-02-17 21:15:37 +0900

    add fn create_config

--- src/app/view/config.rs
+++ src/app/view/config.rs
@@ -5,9 +5,10 @@ use super::*;
 pub(super) fn view(app: &App) -> Element<Message> {
   let theme_pick = pick_list(Theme::ALL, Some(&app.current_theme), Message::ChangeTheme).text_size(12);
 
-  let configs = Column::new()
-    .push(config_item("select theme", theme_pick))
-    .push(config_item("testing notify", button("Send").on_press(Message::Notify)));
+  let configs = create_config(vec![
+    ("select theme", theme_pick.into()),
+    ("testing notify", button("Send").on_press(Message::Notify).into()),
+  ]);
 
   Element::from({
     Column::new()
@@ -34,4 +35,13 @@ fn config_item<'a, Msg: 'a>(
   input: impl Into<Element<'a, Msg>>,
 ) -> Element<'a, Msg> {
   Element::from(Row::new().push(label).push(Space::with_width(Fill)).push(input))
+}
+
+fn create_config<'a, Msg: 'a>(items: Vec<(&'a str, Element<'a, Msg>)>) -> Column<'a, Msg> {
+  let vec = items
+    .into_iter()
+    .map(|(label, element)| config_item(label, element))
+    .collect::<Vec<_>>();
+
+  Column::new().extend(vec.into_iter())
 }
